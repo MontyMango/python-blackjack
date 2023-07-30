f01ecc8967cc20447de55a772fba2d0b7b731ef5
Cam <59061157+MontyMango@users.noreply.github.com>
2023-07-30 00:56:01 +0000

    made deck and deck test Everything seems to work with the deck!

---
+++ deck.py
@@ -0,0 +1,32 @@
+from random import choice
+
+class deck():
+    def __init__(self):
+        self.deck = []
+
+    # Makes the deck
+    def make_deck(self):
+        numbers = ['A',1,2,3,4,5,6,7,8,9,10]
+        symbols = ['Clubs','Hearts','Spades','Diamonds']
+        for number in numbers:
+            for symbol in symbols:
+                self.deck.append([number,symbol])
+
+
+    # Since draw makes a random choice
+    def shuffle_deck(self):
+        self.make_deck()
+
+
+    # Picks a card, and then removes it from the deck.
+    def draw(self):
+        card = choice(self.deck)    # Pick a card from the play deck
+        self.deck.remove(card)      # Remove the card from the play deck
+        return card                 # Return the card to who called it
+    
+
+    # These functions below are for troubleshooting purposes!
+    # Shows the avaliable cards in the play deck
+    def show_play_deck(self):
+        for card in self.deck:
+            print(card)
---
+++ deck_test.py
@@ -0,0 +1,10 @@
+import deck
+
+card_deck = deck.deck()
+card_deck.make_deck()
+print(card_deck.show_play_deck())
+
+drawn_card = card_deck.draw()
+print(str(drawn_card[0]) + " of " + drawn_card[1])
+
+print(card_deck.show_play_deck())
