7d7eab3369fcde609a6506f9059400374ee06c25
Cam <59061157+MontyMango@users.noreply.github.com>
2023-08-12 00:08:36 +0000

    Exiting is now possible though bidding. There is another ending! (oh the lore)

--- main.py
+++ main.py
@@ -16,11 +16,13 @@ def game():
     print("(The man looks at you)")
     print("Hi! Welcome to Blackjack!\n\n")
 
-    while score.get_player_score() > 0:
+    leave = False
+
+    while (score.get_player_score() > 0) and (leave is False):
         # Bidding
-        if(bid()):
+        leave = bid()
+        if(leave):
             print("You leave the table with " + str(score.get_player_score()))
-            print("Rare good ending? I think so.")
         else:
             # Gameplay
             print("Let's begin!\n\n")
@@ -49,19 +51,42 @@ def game():
             gameplay.win_check()
             print("\n\n\n\n\n")
     
-    print("Oh, it looks like you are out of funds... Boys! Get this person out of here!")
-    sleep(dealtime)
-    print("You get up and try to run but the 2 large people come up to you, grab you, and push you outside.")
-    sleep(dealtime)
-    print("Well, there\'s no way home... So I guess I will sleep here...")
-    sleep(dealtime)
-    print("You fell asleep on the concrete outside of the casino...")
-    sleep(dealtime)
-    print("Thanks for playing!")
+    if (score.get_player_score() <= 0):
+        print("Oh, it looks like you are out of funds... Boys! Get this person out of here!")
+        sleep(dealtime)
+        print("You get up and try to run but the 2 large people come up to you, grab you, and push you outside.")
+        sleep(dealtime)
+        print("Well, there\'s no way home... So I guess I will sleep here...")
+        sleep(dealtime)
+        print("You fell asleep on the concrete outside of the casino...")
+        sleep(dealtime)
+        print("Thanks for playing!")
+    else:
+        print("You then see the doors open by two men holding the door for you.")
+        sleep(dealtime)
+        print("You pass the two men holding the door with a smile.")
+        sleep(dealtime)
+        print("You then look in your pockets and see that your winnings.")
+        sleep(dealtime)
+        print("\"Ahh, " + str(score.get_player_score()) + " points... \" You say to yourself")
+        sleep(dealtime)
+        print("What will I do with these points? Buy a fancy car? A house? Or charity?")
+        sleep(dealtime)
+        print("\"Oh I don't know, let\'s just go home and enjoy the evening.\"")
+        sleep(dealtime)
+        print("You get into your car and then start to drive home... The end.")
+        sleep(dealtime)
+        for i in range(5):
+            print(".", end='')
+            sleep(dealtime/2)
+        print(" or is it???")
+        sleep(dealtime*2)
+        print("\nThanks for playing!")
+
 
 
 def bid():
-    print("What is your bid?\n")
+    print("What is your bid? Bid 0 to leave table.\n")
     sleep(dealtime/2)
     print("Current points: " + str(score.get_player_score()))
     has_bid = False
@@ -70,8 +95,12 @@ def bid():
             bid = int(input("Bid: "))
             if (bid > score.get_player_score()):
                 print("...\nI think that you are short in funds... Could you make a bid that you can afford?")
-            elif (bid <= 0):
+            elif (bid < 0):
                 print("...\nI don\'t let people play for free around here. You\'re going to need to make a bid")
+            elif (bid == 0):
+                print("You get up from the table")
+                has_bid = True
+                escape = True
             else:
                 print("Alright, here we go!")
                 score.set_bet(bid)
